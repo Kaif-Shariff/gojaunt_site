1f3cfa08839335aa31d1780ded8687d60de2f063
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-12-28 20:35:11 +0530

    contact and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./core/components/navbar.jsx";
 import HomePage from "./features/home/pages/home.page.jsx";
+import AboutPage from "./features/about/pages/about.page.jsx";
+import ContactPage from "./features/contact/pages/contact.page.jsx";
 
 function App() {
     return (
         <Router>
-            <Navbar />
+            <Navbar/>
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
+                <Route path="/contact" element={<ContactPage/>}/>
             </Routes>
         </Router>
     )
---
+++ src/features/about/pages/about.page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Us Page</h1>
+        </div>
+    );
+}
+
