6e2af79d6eca4e67bf8c93782fdf6145cc46b7d4
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-12-29 20:42:03 +0530

    Update: footer & navbar

--- src/core/components/footer.jsx
+++ src/core/components/footer.jsx
@@ -1,20 +1,27 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear(); // Get the current year
 
     return (
         <footer className="bg-primary text-white py-4">
-            <div className="container mx-auto flex flex-col sm:flex-row sm:justify-between items-center space-y-4 sm:space-y-0">
+            <div
+                className="container mx-auto flex flex-col sm:flex-row sm:justify-between items-center space-y-4 sm:space-y-0">
                 {/* Left Side */}
                 <div className="text-sm font-semibold text-center sm:text-left">
                     <p>© {currentYear} GoJaunt Private Limited</p>
                 </div>
 
                 {/* Center */}
-                <div className="space-y-2 sm:space-x-6 sm:space-y-0 font-semibold text-sm text-center">
-                    <a href="/terms" className="block sm:inline">Terms & Conditions</a>
-                    <a href="/privacy" className="block sm:inline">Privacy Policy</a>
+                <div className="space-y-2 sm:space-x-6 sm:space-y-0 font-semibold lg:flex text-sm text-center">
+                    <Link to="https://kaif-shariff.github.io/gojaunt_privacy/gojaunt_terms.html"
+                          className="block sm:inline">
+                        <p>Terms & Conditions</p>
+                    </Link>
+                    <Link to="https://kaif-shariff.github.io/gojaunt_privacy/gojaunt_terms.html"
+                          className="block sm:inline">
+                        <p>Privacy Policy</p>
+                    </Link>
                 </div>
 
                 {/* Right Side */}
