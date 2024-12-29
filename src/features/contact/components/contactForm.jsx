6162b52969ee6c837c17be720acf4c23088588c2
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-12-29 19:47:18 +0530

    Update: Contact form UI

--- src/features/contact/components/contactForm.jsx
+++ src/features/contact/components/contactForm.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
@@ -10,35 +10,35 @@ const validationSchema = Yup.object({
         .required('Phone Number is required'),
 });
 
-const InputField = ({ label, name, type }) => (
+const InputField = ({label, name, type}) => (
     <div className="flex flex-col">
         <label htmlFor={name} className="text-sm font-medium">{label}</label>
-        <Field name={name} type={type} className="border border-gray-300 p-2 rounded-md" />
-        <ErrorMessage name={name} component="div" className="text-red-500 text-sm" />
+        <Field name={name} type={type} className="border border-gray-300 p-2 rounded-md"/>
+        <ErrorMessage name={name} component="div" className="text-red-500 text-sm"/>
     </div>
 );
 
 const ContactForm = () => {
     return (
         <Formik
-            initialValues={{ firstName: '', lastName: '', email: '', phoneNumber: '' }}
+            initialValues={{firstName: '', lastName: '', email: '', phoneNumber: ''}}
             validationSchema={validationSchema}
             onSubmit={(values) => {
                 console.log(values);
             }}
         >
-            {({ handleSubmit }) => (
-                <Form onSubmit={handleSubmit} className="space-y-4 p-4 lg:w-[28rem]">
+            {({handleSubmit}) => (
+                <Form onSubmit={handleSubmit} className="space-y-4 lg:w-[28rem]">
                     <div className="flex flex-col md:flex-row md:space-x-4">
-                        <InputField label="First Name" name="firstName" type="text" />
-                        <InputField label="Last Name" name="lastName" type="text" />
+                        <InputField label="First Name" name="firstName" type="text"/>
+                        <InputField label="Last Name" name="lastName" type="text"/>
                     </div>
-                    <InputField label="Email" name="email" type="email" />
-                    <InputField label="Phone Number" name="phoneNumber" type="text" />
+                    <InputField label="Email" name="email" type="email"/>
+                    <InputField label="Phone Number" name="phoneNumber" type="text"/>
                     <div className="flex justify-center">
                         <button
                             type="submit"
-                            className="bg-primary text-white py-2 px-4 w-full rounded-lg hover:bg-primary-dark focus:outline-none"
+                            className="bg-primary text-white mb-10 lg:mb-0 py-2 px-4 w-full rounded-lg hover:bg-primary-dark focus:outline-none"
                         >
                             Submit
                         </button>
--- src/features/contact/components/formContainer.jsx
+++ src/features/contact/components/formContainer.jsx
@@ -4,13 +4,13 @@ const FormContainer = () => {
     return (
         <div className="flex flex-col lg:flex-row">
             <div className="flex-1 px-4">
-                <div>
-                    <h1 className="text-2xl">Get in touch</h1>
+                <div className="mb-8">
+                    <h1 className="text-2xl font-semibold">Get in touch</h1>
                     <h3 className="text-md lg:text-lg">Our team would love to hear from you</h3>
                 </div>
-                <ContactForm />
+                <ContactForm/>
             </div>
-            <div className="bg-purple-500 hidden w-[28rem] h-[30rem] lg:flex"></div>
+            <div className="bg-purple-500 hidden w-[28rem] h-[30rem] m-4 lg:flex"></div>
         </div>
     );
 };
